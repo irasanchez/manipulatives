d0250bb3d0fbfff5e79b2e059780c4e76c4796fb
Ira Sanchez <ira.d.sanchez@gmail.com>
2022-04-05 17:05:07 -0700

    Reset

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,7 @@
 import React, { useState } from "react";
 
-import { Values } from ".";
-
 function App() {
-    let demos = ["for ( let i = 0; i < 10; i++ ) { console.log(i); }"];
-    let [demoOptions, setDemoOptions] = useState(demos);
-
-    return (
-        <div className="App">
-            test
-            <Values />
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
