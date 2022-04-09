a50f0128bd537e03a34ea85edd87c04279e824ea
Ira Sanchez <ira.d.sanchez@gmail.com>
2022-04-08 17:42:01 -0700

    Removes extra App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import React, { useReducer } from "react";
-import ReactDOM from "react-dom";
-import { Layout, ForLoop } from "./components";
-import {
-    ForLoopContext,
-    initialForLoopState,
-    reducer,
-    ACTIONS,
-} from "./lib/state";
+import React, { useState } from "react";
+
 function App() {
-    const [state, dispatch] = useReducer(reducer, initialForLoopState);
-    return (
-        <div className="App">
-            <Layout>
-                <ForLoopContext.Provider value={{ state, dispatch, ACTIONS }}>
-                    <ForLoop />
-                </ForLoopContext.Provider>
-            </Layout>
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
