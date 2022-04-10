d089aea8f0d3ab38472ab48831323382dcd554f6
Ira Sanchez <ira.d.sanchez@gmail.com>
2022-04-10 09:03:08 -0700

    Adds fix to Values tests
    - minor typo

--- src/components/Values/Values.test.js
+++ src/components/Values/Values.test.js
@@ -4,7 +4,6 @@ describe("Values component is working", () => {
     it("is rendered at the right route", () => {
         mounter();
         goToRoute("Values");
-        cy.url;
         cy.contains(".Values h1", "Values");
         cy.url().should("include", "/values");
     });
